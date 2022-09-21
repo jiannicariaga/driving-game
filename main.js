f4241b91dce8c3e2fcb2f3b3d8b445e93dc15f80
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-21 20:55:14 +0000

    Created an event listener for directional keys and updated the images styling to reflect key pressedn. #2

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-var vehicleDirection = {
-  up: 'turn-up',
-  right: 'turn-right',
-  down: 'turn-down',
-  left: 'turn-left'
+var $vehicle = document.querySelector('.vehicle');
+var direction = {
+  up: 'vehicle turn-up',
+  right: 'vehicle turn-right',
+  down: 'vehicle turn-down',
+  left: 'vehicle turn-left'
 };
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'w' || event.keyCode === 38) {
+    $vehicle.className = direction.up;
+  } else if (event.key === 'd' || event.keyCode === 39) {
+    $vehicle.className = direction.right;
+  } else if (event.key === 's' || event.keyCode === 40) {
+    $vehicle.className = direction.down;
+  } else if (event.key === 'a' || event.keyCode === 37) {
+    $vehicle.className = direction.left;
+  }
+});

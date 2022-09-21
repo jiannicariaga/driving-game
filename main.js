2c3f00b1a39d4556ab891138508c98cc34abf79f
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-21 22:50:00 +0000

    Added an event listenter to start the vehicle and update its x coordinates every 16ms. #3

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ var direction = {
   north: 'vehicle direction-north',
   south: 'vehicle direction-south',
   east: 'vehicle direction-east',
-  west: 'vehicle direction-west',
-  locationX: 0,
-  locationY: 0
+  west: 'vehicle direction-west'
+};
+var coordinates = {
+  x: 0,
+  y: 0
 };
 
 document.addEventListener('keydown', function (event) {
@@ -18,4 +20,13 @@ document.addEventListener('keydown', function (event) {
   } else if (event.key === 'a' || event.keyCode === 37) {
     $vehicle.className = direction.west;
   }
+
+  if (event.key === ' ') {
+    setInterval(startVehicle, 16);
+  }
 });
+
+function startVehicle() {
+  coordinates.x += 10;
+  $vehicle.style.left = coordinates.x + 'px';
+}

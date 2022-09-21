c0bec57e162d7aa26e139f3ac71880e119d1856c
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-21 23:47:14 +0000

    Added Boolean variable to data model and updated callback function logic. #4

--- main.js
+++ main.js
@@ -1,32 +1,44 @@
 var $vehicle = document.querySelector('.vehicle');
-var direction = {
-  north: 'vehicle direction-north',
-  south: 'vehicle direction-south',
-  east: 'vehicle direction-east',
-  west: 'vehicle direction-west'
+var vehicle = {
+  direction: {
+    north: 'vehicle direction-north',
+    south: 'vehicle direction-south',
+    east: 'vehicle direction-east',
+    west: 'vehicle direction-west'
+  },
+  coordinates: {
+    x: 0,
+    y: 0
+  },
+  engine: {
+    on: false
+  }
 };
-var coordinates = {
-  x: 0,
-  y: 0
-};
+var intervalId = null;
 
 document.addEventListener('keydown', function (event) {
   if (event.key === 'w' || event.keyCode === 38) {
-    $vehicle.className = direction.north;
+    $vehicle.className = vehicle.direction.north;
   } else if (event.key === 's' || event.keyCode === 40) {
-    $vehicle.className = direction.south;
+    $vehicle.className = vehicle.direction.south;
   } else if (event.key === 'd' || event.keyCode === 39) {
-    $vehicle.className = direction.east;
+    $vehicle.className = vehicle.direction.east;
   } else if (event.key === 'a' || event.keyCode === 37) {
-    $vehicle.className = direction.west;
+    $vehicle.className = vehicle.direction.west;
   }
 
   if (event.key === ' ') {
-    setInterval(startVehicle, 16);
+    vehicle.engine.on = !vehicle.engine.on;
+
+    if (vehicle.engine.on) {
+      intervalId = setInterval(startEngine, 16);
+    } else {
+      clearInterval(intervalId);
+    }
   }
 });
 
-function startVehicle() {
-  coordinates.x += 10;
-  $vehicle.style.left = coordinates.x + 'px';
+function startEngine() {
+  vehicle.coordinates.x += 10;
+  $vehicle.style.left = vehicle.coordinates.x + 'px';
 }
